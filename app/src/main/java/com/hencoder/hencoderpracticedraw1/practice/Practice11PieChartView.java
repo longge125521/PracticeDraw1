a951caf72c2808a2aa7847c8a1ef9b566bbc3324
longge125521 <longge125521@126.com>
2017-09-22 15:28:13 +0800

    提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,11 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice11PieChartView extends View {
+
+    private Paint mPaint;
 
     public Practice11PieChartView(Context context) {
         super(context);
@@ -23,8 +26,9 @@ public class Practice11PieChartView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        mPaint = new Paint();
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        canvas.drawCircle(200, 200, 200, mPaint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,12 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
-
+    private Paint mPaint;
     public Practice2DrawCircleView(Context context) {
         super(context);
     }
@@ -26,5 +28,18 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        mPaint = new Paint();
+        mPaint.setColor(Color.DKGRAY);
+        mPaint.setAntiAlias(true);
+//        练习内容：使用 canvas.drawColor() 方法把 View 涂成黄色
+//        黄色： Color.YELLOW
+        canvas.drawCircle(200, 200, 100, mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);//划线模式
+        canvas.drawCircle(400, 200, 100, mPaint);
+        mPaint.setStrokeWidth(20);
+        canvas.drawCircle(400, 400, 100, mPaint);
+        mPaint.setStyle(Paint.Style.FILL);//填充模式
+        mPaint.setColor(Color.parseColor("#c08a8a"));
+        canvas.drawCircle(200, 400, 100, mPaint);
     }
 }
